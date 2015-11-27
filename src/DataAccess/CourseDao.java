ac43b45f4693ce460df6fda57a6ad998f4360572
Gaurav Patel <gauravpatel2016@gmail.com>
2015-11-26 23:13:41 -0700

    CourseDao StudentDao

--- src/DataAccess/CourseDao.java
+++ src/DataAccess/CourseDao.java
@@ -1,6 +1,21 @@
 package DataAccess;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import DataTransfer.CourseDto;
+
 public class CourseDao {
 
+   private Map<String, CourseDto> courseObject = new HashMap<>();
+    
+    public void insert(final CourseDto course) {
+        courseObject.put(course.getCourseId(), course);
+    }
+    
+    public CourseDto getById(final String courseId) {
+        return courseObject.get(courseId);
+        
+    }
 
 }
--- src/DataAccess/StudentDao.java
+++ src/DataAccess/StudentDao.java
@@ -1,5 +1,26 @@
 package DataAccess;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import DataTransfer.StudentDto;
+
+
 public class StudentDao {
+    private Map<String, StudentDto> studentObject = new HashMap<>();
+    
+    public void insert(final StudentDto student) {
+    	studentObject.put(student.getUserId(), student);
+    }
+    
+    public StudentDto getById(final String studentId) {
+        return studentObject.get(studentId);
+        
+    }
+    
+    public Collection<StudentDto> getAll(){
+        return studentObject.values();
+    }
 
 }
