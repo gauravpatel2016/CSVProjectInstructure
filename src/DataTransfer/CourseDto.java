22e01cb7feae47f8287876d3ce71fcc34da37a63
Gaurav Patel <gauravpatel2016@gmail.com>
2015-11-26 21:28:50 -0700

    Class Variable created - CourseDto

--- src/DataTransfer/CourseDto.java
+++ src/DataTransfer/CourseDto.java
@@ -2,4 +2,32 @@ package DataTransfer;
 
 public class CourseDto {
 
+	/*
+	 * 
+	 * course columns:     course_id, course_name, state
+	 * student columns:    user_id,   user_name, course_id, state
+	 */
+	
+    private String course_id;
+    private String course_name;
+    private String course_state;
+    
+    public String getId() {
+        return this.course_id;
+    }
+    public void setId(String id) {
+        this.course_id = id;
+    }
+    public String getName() {
+        return this.course_name;
+    }
+    public void setName(String name) {
+        this.course_name = name;
+    }
+    public String getState() {
+        return this.course_state;
+    }
+    public void setState(String state) {
+        this.course_state = state;
+    }
 }
